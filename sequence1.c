a2398ed1aa6e54f30f6efab926d01ce1247d3172
PrashantBalasahebMungase <prashant.mungase99@gmail.com>
2025-06-10 23:18:28 +0530

    Add files via upload

---
+++ sequence1.c
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+//  File Name   :   sequence1.c
+//  Description :  Demonstrates string initialization and printing using character arrays
+//  Author      :  Prashant Balasaheb Mungase
+//  Date        :  10/06/2025
+//  Input       :  None
+//  Output      :  Prints strings stored in character arrays
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+    Start 
+
+            Step 1 : Include the stdio.h header file.
+
+            Step 2 : Define the main() function.
+
+            Step 3 : Print "Jay Ganesh" four times using printf().
+
+    End 
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh\n");
+    printf("Jay Ganesh\n");
+    printf("Jay Ganesh\n");
+    printf("Jay Ganesh\n");
+
+    return 0;
+}
---
+++ sequence2.c
@@ -0,0 +1,38 @@
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+//  File Name   :  sequence2.c
+//  Description :  This program prints numbers from 1 to 5 using the printf() function.
+//  Author      :  Prashant Balasaheb Mungase
+//  Date        :  10/06/2025
+//  Input       :  None
+//  Output      :  Prints numbers 1 to 5 on separate lines
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+    Start 
+
+                   Step 1 : Include the header file stdio.h.
+
+                    Step 2 : Define the main() function.
+
+                    Step 3 : Use printf() to print numbers 1 to 5,new line.
+
+                    Step 4 : Return 0 to indicate successful execution.
+
+
+    End
+ */    
+#include<stdio.h>
+int main()
+{
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+
+    return 0;
+}
